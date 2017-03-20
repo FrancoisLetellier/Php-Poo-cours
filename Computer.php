44936eedfe2ac5efc37aad9a263a4bde5ca98f45
François LETELLIER <f.letellier0@gmail.com>
2017-03-20 11:18:50 +0100

    mise static

--- Computer.php
+++ Computer.php
@@ -13,10 +13,12 @@ class Computer
 {
 
     private $procType;
+    public static $count = 0;
 
     public function __construct($procType = "Unknown processor")
     {
         $this->setProcType($procType);
+        self::$count++;
     }
 
     public function setProcType($procType)
@@ -33,5 +35,11 @@ class Computer
        return $this->procType;
    }
 
-
+    public function getInfos()
+    {
+        echo "---------------" . PHP_EOL;
+        echo "Computer number " . self::$count . PHP_EOL;
+        echo " processor : " . $this->getProcType() . PHP_EOL;
+        echo "---------------" . PHP_EOL;
+    }
 }
--- test.php
+++ test.php
@@ -8,8 +8,7 @@ $computer2 = new \wcs\Computer("Motorola");
 $computer3 = new \wcs\Computer("");
 $computer4 = new \wcs\Computer();
 
-echo $computer1->getProctype() . PHP_EOL;
-echo $computer2->getProctype() . PHP_EOL;
-echo $computer3->getProctype() . PHP_EOL;
-echo $computer4->getProctype() . PHP_EOL;
-
+echo $computer1->getInfos();
+echo $computer2->getInfos();
+echo $computer3->getInfos();
+echo $computer4->getInfos();
