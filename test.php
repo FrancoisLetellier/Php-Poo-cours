375a3876b9dac2941ed1b7d951e15860025b7964
François LETELLIER <f.letellier0@gmail.com>
2017-03-20 11:22:53 +0100

    mise static count

--- test.php
+++ test.php
@@ -4,11 +4,11 @@
 require "Computer.php";
 
 $computer1 = new \wcs\Computer("Intel");
-$computer2 = new \wcs\Computer("Motorola");
-$computer3 = new \wcs\Computer("");
-$computer4 = new \wcs\Computer();
-
 echo $computer1->getInfos();
+$computer2 = new \wcs\Computer("Motorola");
 echo $computer2->getInfos();
+$computer3 = new \wcs\Computer("");
 echo $computer3->getInfos();
+$computer4 = new \wcs\Computer();
 echo $computer4->getInfos();
+
